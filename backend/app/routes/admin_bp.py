30a386a0dcaf794bc17fc6e3d01eb543cecd664c
hermannwi <hermann-wigers@hotmail.com>
2025-07-28 14:37:06 +0200

    added get_all_products route

--- backend/app/routes/admin_bp.py
+++ backend/app/routes/admin_bp.py
@@ -1,14 +1,14 @@
-# from flask import Blueprint
-# from backend.app.models.diagnostic import Diagnostic
-# from backend.app.models.product import Product
-# from backend.app.models.question import Question
-# from backend.app.models.root_cause import RootCause
-# from backend.app.models.system_version import SystemVersion
+from flask import Blueprint
+from backend.app.models.diagnostics_8d import Diagnostics8d
+from backend.app.models.product import Product
+from backend.app.models.question import Question
+from backend.app.models.root_cause import RootCause
+from backend.app.models.system_version import SystemVersion
 
-# admin = Blueprint("admin", __name__, url_prefix="/admin")
-
-
+admin = Blueprint("admin", __name__, url_prefix="/admin")
 
 
-# @admin_bp.route('/products', methods=['GET'])
-# def get_all_products():
+
+
+@admin_bp.route('/products', methods=['GET'])
+def get_all_products():
