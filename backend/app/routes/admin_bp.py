842feed3d4b29b74af0309f0f3ac0056b4763c81
hermannwi <hermann-wigers@hotmail.com>
2025-07-28 14:42:33 +0200

    m

--- backend/app/routes/admin_bp.py
+++ backend/app/routes/admin_bp.py
@@ -1,14 +1,15 @@
-from flask import Blueprint
+from backend.app import db
+from flask import Blueprint, jsonify
 from backend.app.models.diagnostics_8d import Diagnostics8d
 from backend.app.models.product import Product
 from backend.app.models.question import Question
 from backend.app.models.root_cause import RootCause
 from backend.app.models.system_version import SystemVersion
 
-admin = Blueprint("admin", __name__, url_prefix="/admin")
-
-
+admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
 @admin_bp.route('/products', methods=['GET'])
 def get_all_products():
+    products = Product.query.all()
+    return jsonify([{'id': p.id, 'product': p.product, "created_at": p.created_at, "updated_at": p.updated_at} for p in products]) 
