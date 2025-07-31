eafb8f4a986bf79bea67c144c56a5a1b7cee6aab
hermannwi <hermann-wigers@hotmail.com>
2025-07-31 14:55:56 +0200

    added test for relationships in 8d

--- backend/tests/test_diagnostic.py
+++ backend/tests/test_diagnostic.py
@@ -1,5 +1,6 @@
 from backend.app.models.diagnostics_8d import Diagnostics8d
 from backend.app.models.product import Product
+from backend.app.models.root_cause import RootCause
 from backend.app import db
 from datetime import datetime
 
@@ -24,20 +25,38 @@ def test_insert_one(app):
         print(f"first record id = {first_record.id}")
 
 def insert_one():
-    new_record = Diagnostics8d(product_id=1, issue="some issue", created_at=datetime.now(), updated_at=datetime.now())
-    db.session.add(new_record)
-    db.session.flush()
+    
+        new_record = Diagnostics8d(product_id=1, issue="some issue", created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(new_record)
+        db.session.flush()
 
 
-def test_backref(client):
-    product1 = Product(product='EML200', created_at=datetime.now(), updated_at=datetime.now())
-    db.session.add(product1)
-    db.session.commit()
+def test_backref(app):
+    with app.app_context():
+        product1 = Product(product='EML200', created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(product1)
+        db.session.commit()
 
-    d_8d1 = Diagnostics8d(product_id=product1.id, issue='some issue', created_at=datetime.now(), updated_at=datetime.now())
-    d_8d2 = Diagnostics8d(product_id=product1.id, issue='some other issue', created_at=datetime.now(), updated_at=datetime.now())
-    db.session.add(d_8d1)
-    db.session.add(d_8d2)
-    db.session.commit()
+        d_8d1 = Diagnostics8d(product_id=product1.id, issue='some issue', created_at=datetime.now(), updated_at=datetime.now())
+        d_8d2 = Diagnostics8d(product_id=product1.id, issue='some other issue', created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(d_8d1)
+        db.session.add(d_8d2)
+        db.session.commit()
 
-    print(db.session.get(Product, product1.id).diagnostic8ds)
+        print(db.session.get(Product, product1.id).diagnostic8ds)
+
+
+def test_relationships(app):
+    with app.app_context():
+        product1 = Product(product='EML200', created_at=datetime.now(), updated_at=datetime.now())
+        root_cause1 = RootCause(root_cause='a root cause', created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(product1)
+        db.session.add(root_cause1)
+        db.session.commit()
+
+        d_8d1 = Diagnostics8d(product_id=product1.id, issue='some issue', root_cause_id=root_cause1.id, created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(d_8d1)
+        db.session.commit()
+
+        assert  (db.session.get(Diagnostics8d, d_8d1.id).product.product == 'EML200')
+        assert  (db.session.get(Diagnostics8d, d_8d1.id).root_cause.root_cause == 'a root cause')
